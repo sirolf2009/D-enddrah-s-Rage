3b7871b7d5a6403aadf4148e549bb60ac24ef9b1
sirolf2009 <masterflappie@gmail.com>
2025-01-24 21:28:56 +0200

    add terrain, pick up bread

---
+++ src/main/java/com/dendrrahsrage/control/FoodControl.kt
@@ -0,0 +1,12 @@
+package com.dendrrahsrage.control
+
+import com.jme3.renderer.RenderManager
+import com.jme3.renderer.ViewPort
+import com.jme3.scene.control.AbstractControl
+
+class FoodControl: AbstractControl() {
+    override fun controlUpdate(tpf: Float) {
+    }
+    override fun controlRender(rm: RenderManager?, vp: ViewPort?) {
+    }
+}
